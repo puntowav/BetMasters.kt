20578a0b4bd561a0e467173b048ef654449d1001
Marc <wavpunto@gmail.com>
2024-11-30 17:26:19 +0100

    Login Done "falta el logo"

--- app/src/main/java/com/example/betmasters/Home.kt
+++ app/src/main/java/com/example/betmasters/Home.kt
@@ -9,8 +9,6 @@ import androidx.core.view.WindowInsetsCompat
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
