6d4ce596616d8228b9456e78fbbbf91ed5a3e996
alex <alexvictorb@gmail.com>
2024-12-04 18:56:55 +0100

    miodo pantalla copleta

--- app/src/main/java/com/example/betmasters/Home.kt
+++ app/src/main/java/com/example/betmasters/Home.kt
@@ -2,6 +2,7 @@ package com.example.betmasters
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
@@ -21,8 +22,11 @@ class Home : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.home)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
 
         val openSheetButton: CardView = findViewById(R.id.card_filter_fav)
 
--- app/src/main/java/com/example/betmasters/Redeem.kt
+++ app/src/main/java/com/example/betmasters/Redeem.kt
@@ -2,6 +2,7 @@ package com.example.betmasters
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
@@ -14,6 +15,10 @@ class Redeem : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_redeem)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
 
         val homeItem: LinearLayout = findViewById(R.id.iconHome)
         val redeemItem: LinearLayout = findViewById(R.id.iconRedeem)
