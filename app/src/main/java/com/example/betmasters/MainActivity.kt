cacfce86a8becab89a8032321a74f09b201c374e
Marc <wavpunto@gmail.com>
2024-12-06 19:40:48 +0100

    new changes

--- app/src/main/java/com/example/betmasters/MainActivity.kt
+++ app/src/main/java/com/example/betmasters/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.betmasters
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -13,13 +14,14 @@ import androidx.preference.PreferenceManager
 
 class MainActivity : AppCompatActivity() {
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        // Obtén la preferencia de modo noche y aplica el tema
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+
         val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this)
         val isNightMode = sharedPrefs.getBoolean("nightMode", false)
         if (isNightMode) {
