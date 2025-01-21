ac45ab3c0cb314e195516e5060dc6a2a66912690
alex <alexvictorb@gmail.com>
2025-01-21 18:32:58 +0100

    lateral drawer menu hecho y totalmente funcional

--- app/src/main/java/com/example/betmasters/Main.kt
+++ app/src/main/java/com/example/betmasters/Main.kt
@@ -4,21 +4,32 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import com.example.betmasters.HomeFragment.Companion.coins
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.internal.NavigationMenu
+import com.google.android.material.navigation.NavigationView
 
 class Main : AppCompatActivity(), DialogCallBack {
     private lateinit var txtCoins : TextView
+    private lateinit var txtCoinMenu : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
 
+        val drawer_menu: NavigationView = findViewById(R.id.drawer_menu)
+        val drawerLayout: DrawerLayout = findViewById(R.id.main)
         val profileButton: ImageButton = findViewById(R.id.btnPerfil)
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        val headerView = drawer_menu.getHeaderView(0) // Obtén el diseño inflado del menú
+        val cvNavigate = headerView.findViewById<androidx.cardview.widget.CardView>(R.id.cvProfile)
+        txtCoinMenu = headerView.findViewById(R.id.txtCoinsDrawer)
         txtCoins = findViewById(R.id.txtCoins)
 
         updateCoins()
@@ -31,6 +42,14 @@ class Main : AppCompatActivity(), DialogCallBack {
         }
 
         profileButton.setOnClickListener {
+            if (drawerLayout.isDrawerOpen(drawer_menu)) {
+                drawerLayout.closeDrawer(drawer_menu) // Cierra el Drawer si está abierto
+            } else {
+                drawerLayout.openDrawer(drawer_menu) // Abre el Drawer si está cerrado
+            }
+        }
+
+        /*cvNavigate.setOnClickListener {
             val intent = Intent(this, SettingsActivity::class.java)
             val selectedItemId = bottomNavigationView.selectedItemId
             val source = when(selectedItemId){
@@ -41,7 +60,38 @@ class Main : AppCompatActivity(), DialogCallBack {
             }
             intent.putExtra("source", "home")
             startActivity(intent)
+        }*/
+
+        cvNavigate.setOnClickListener {
+            val intent = Intent(this, Edit_profile::class.java)
+            startActivity(intent)
         }
+
+        drawer_menu.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.profile -> {
+                    val intent = Intent(this, Edit_profile::class.java)
+                    startActivity(intent)
+                }
+                R.id.settings -> {
+                    val intent = Intent(this, SettingsActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.titulos -> {
+                    showToast("Titulos seleccionado")
+                }
+                R.id.tienda -> {
+                    showToast("Tienda seleccionado")
+                }
+                else -> {
+                    showToast("Opción no reconocida")
+                }
+            }
+            // Cerrar el Drawer después de seleccionar una opción
+            drawerLayout.closeDrawer(GravityCompat.START)
+            true
+        }
+
 
         bottomNavigationView.setOnNavigationItemSelectedListener { item ->
             var selectedFragment: Fragment? = null
@@ -63,10 +113,15 @@ class Main : AppCompatActivity(), DialogCallBack {
     fun updateCoins() {
         val formattedCoins = String.format("%.0f", coins)
         txtCoins.text = "$formattedCoins"
+        txtCoinMenu.text = "$formattedCoins"
         //saveCoinsToPreferences()
     }
 
     override fun onCoinsUpdated() {
         updateCoins()
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
